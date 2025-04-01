432c7a49de49fdc33ed7be1d20718cc539cbfcae
b166er <delagneauadrien@yahoo.fr>
2025-04-01 23:28:00 +0200

    first commit

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,18 +1,14 @@
 import { PrismaClient } from "@prisma/client";
 
-const prismaClientSingleton = () => {
-  return new PrismaClient();
-};
-
-type PrismaClientSingleton = ReturnType<typeof prismaClientSingleton>;
-
 const globalForPrisma = globalThis as unknown as {
-  prisma: PrismaClientSingleton | undefined;
+  prisma: PrismaClient | undefined;
 };
 
-const db = globalForPrisma.prisma ?? prismaClientSingleton();
+const db = globalForPrisma.prisma ?? new PrismaClient();
+
+if (process.env.NODE_ENV !== "production") {
+  globalForPrisma.prisma = db;
+}
 
 export default db;
 
-if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = db;
-// test
