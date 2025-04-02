29dfddc50774ec10aee957b7fbfb562d03a06fb8
b166er <delagneauadrien@yahoo.fr>
2025-04-02 09:16:15 +0200

    commit

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,14 +1,15 @@
+import { Suspense } from "react";
 import { GetCategory } from "@/actions/caterogy-action";
 import FilterCarousel from "@/components/filter-carousel";
 
-
-
 const HomePage = async () => {
-
   const categories = await GetCategory();
+
   return (
     <div>
-     <FilterCarousel categories={categories}/>
+      <Suspense fallback={<div>Loading filters...</div>}>
+        <FilterCarousel categories={categories} />
+      </Suspense>
     </div>
   );
 };
